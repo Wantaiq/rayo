ae88d2651f3b8a7a2f754fd8a66ca1dbbee55927
wantiq <99611999+Wantaiq@users.noreply.github.com>
2024-11-15 14:51:26 +0100

    ci: extend error handler to handle ValidationError

--- server/src/middlewares/errorHandler.ts
+++ server/src/middlewares/errorHandler.ts
@@ -1,9 +1,15 @@
 import AppError from '@/utils/AppError';
 import { ErrorRequestHandler } from 'express';
+import { ValidationError } from 'yup';
 
 const errorHandler: ErrorRequestHandler = (error, _req, res, _next) => {
+  if (error instanceof ValidationError) {
+    res.status(400).json({ error: error.errors[0] }).end();
+    return;
+  }
+
   if (error instanceof AppError) {
-    res.status(error.statusCode).json({ error: error.message });
+    res.status(error.statusCode).json({ error: error.message }).end();
     return;
   }
 
