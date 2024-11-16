7ea192d7e8c60cf2d89f41d54819fe983c50e78b
wantiq <99611999+Wantaiq@users.noreply.github.com>
2024-11-16 20:43:55 +0100

    component: add default Card component

---
+++ client/src/components/AppCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex flex-col p-10 w-fit rounded-lg border border-zinc-800">
+    <div v-if="$slots.header">
+      <slot name="header" />
+    </div>
+    <div v-if="$slots.body">
+      <slot name="body" />
+    </div>
+    <div
+      v-if="$slots.footer"
+      class="mt-auto">
+      <slot name="footer" />
+    </div>
+  </div>
+</template>
