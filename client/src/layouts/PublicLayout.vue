03172a9221ad623c36688a557fb85f14a1b6586a
wantiq <99611999+Wantaiq@users.noreply.github.com>
2024-11-16 18:11:18 +0100

    component: add public layout

---
+++ client/src/layouts/PublicLayout.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  import AppHeader from '@/components/AppHeader.vue';
+  import PublicNavigation from '@/components/PublicNavigation.vue';
+</script>
+
+<template>
+  <AppHeader>
+    <template #navigation>
+      <PublicNavigation />
+    </template>
+  </AppHeader>
+  <main class="container mx-auto">
+    <slot />
+  </main>
+</template>
