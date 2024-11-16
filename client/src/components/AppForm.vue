3b94b783029dbf6c42ad0d4f87ebaa8eef9d2034
wantiq <99611999+Wantaiq@users.noreply.github.com>
2024-11-16 18:43:32 +0100

    component: add default app form component

---
+++ client/src/components/AppForm.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+  import type { SubmissionHandler } from 'vee-validate';
+  import { Form as VeeForm } from 'vee-validate';
+  import { toRefs } from 'vue';
+  import type { AnyObjectSchema } from 'yup';
+
+  type FormPropsType = {
+    validationSchema: AnyObjectSchema;
+    onSubmit: SubmissionHandler;
+  };
+
+  const props = defineProps<FormPropsType>();
+  const { validationSchema, onSubmit } = toRefs(props);
+</script>
+
+<template>
+  <VeeForm
+    @submit="onSubmit"
+    :validation-schema="validationSchema">
+    <slot name="fields" />
+    <slot name="actions" />
+  </VeeForm>
+</template>
