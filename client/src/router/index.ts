d2eb141ae45b14f391b41b5805170dc045c53c70
wantiq <99611999+Wantaiq@users.noreply.github.com>
2024-11-17 13:05:53 +0100

    feature: add navigation guards

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,3 +1,4 @@
+import { useUserStore } from '@/stores/user';
 import { createRouter, createWebHistory } from 'vue-router';
 import routes from './routes';
 
@@ -6,4 +7,17 @@ const router = createRouter({
   routes,
 });
 
+router.beforeEach(to => {
+  const userStore = useUserStore();
+
+  if (to.meta.requiresAuth && !userStore.isLoggedIn) {
+    return { name: 'login', replace: true };
+  }
+
+  if (userStore.isLoggedIn) {
+    if (to.name === 'login' || to.name === 'register') {
+      return { name: 'home', replace: true };
+    }
+  }
+});
 export default router;
